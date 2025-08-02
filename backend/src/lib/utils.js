81a169416a9dd7082c38b16a2f46ea0f7ed1860c
z4fwan <zn4.studio@gmail.com>
2025-08-03 00:50:32 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,10 +6,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
 
   return token;
