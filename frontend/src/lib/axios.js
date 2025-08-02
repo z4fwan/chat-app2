41f8ac3b10459d3d00930b5358cf5aa201716e1d
z4fwan <zn4.studio@gmail.com>
2025-08-02 22:41:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"                  // ✅ Dev server
-      : import.meta.env.VITE_API_BASE_URL,           // ✅ Prod server (do not append `/api`)
-  withCredentials: true,                             // ✅ Send cookies (JWT)
+      ? "http://localhost:5001/api"
+      : import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
